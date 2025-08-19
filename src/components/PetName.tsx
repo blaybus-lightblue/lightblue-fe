db9b8f6f77811fdd384bd817d9605a9c8005c755
korkt-kim <gnscott6@naver.com>
2025-08-19 17:01:42 +0900

    zustand 설정 예시

--- src/components/PetName.tsx
+++ src/components/PetName.tsx
@@ -1,9 +1,25 @@
 'use client'
 
 import { useGetPetById } from '@/apis/pets/usePetService'
+import { useCounterStore } from '@/providers/CounterStoreProvider'
 
 export const PetName = () => {
   const { data } = useGetPetById({ petId: 4 })
+  const { count, incrementCount, decrementCount } = useCounterStore(
+    state => state
+  )
 
-  return <>{data?.data.name}</>
+  return (
+    <>
+      {data?.data.name}
+      Count: {count}
+      <hr />
+      <button type='button' onClick={incrementCount}>
+        Increment Count
+      </button>
+      <button type='button' onClick={decrementCount}>
+        Decrement Count
+      </button>
+    </>
+  )
 }
